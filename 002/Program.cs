042924816fd5bb73e1a9a058f97aa62c4220c5f8
Elena.Smagina <esmagina17@gmail.com>
2022-12-03 12:38:17 +0500

    002

---
+++ 002/Program.cs
@@ -0,0 +1,19 @@
+﻿// int[,] matrix = new int[3, 4];
+// for (int i = 0; i < 3; i++)
+// {
+//  for (int j = 0; j < 4; j++)
+//  {
+//  Console.Write($"{matrix[i, j]} ");
+//  }
+// Console.WriteLine();
+// }
+
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+ for (int j = 0; j < matrix.GetLength(1); j++)
+ {
+ Console.Write($"{matrix[i, j]} ");
+ }
+Console.WriteLine();
+}
